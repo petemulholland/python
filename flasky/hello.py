678869c6ea2670d3fb1d75e451098bb65f8c5203
petemulholland <peter.mulholland@yahoo.co.uk>
2015-03-09 20:43:29 +0000

    flasky 2c

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
 
 @app.route('/')
 def index():
@@ -10,5 +14,5 @@ def user(name):
 	return '<h1>Hello, %s!</h1>' % name
 	
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
 	
