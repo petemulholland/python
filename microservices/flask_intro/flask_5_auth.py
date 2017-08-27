33cac86e3f264464de2841a42f4a08cbb9f03687
Peter Mulholland <peter.mulholland@yahoo.co.uk>
2017-08-27 15:25:43 +0100

    flask globals

---
+++ microservices/flask_intro/flask_5_auth.py
@@ -0,0 +1,56 @@
+from pprint import pprint
+from flask import Flask, jsonify, request
+from werkzeug.routing import BaseConverter, ValidationError
+
+_USERS = {'1': 'Tarek', '2': 'Freya'}
+_IDS = {val: id for id, val in _USERS.items()}
+
+
+class RegisteredUser(BaseConverter):
+    def to_python(self, value):
+        if value in _USERS:
+            return _USERS[value]
+        raise ValidationError()
+
+    def to_url(self, value):
+        return _IDS[value]
+
+
+app = Flask(__name__)
+app.url_map.converters['registered'] = RegisteredUser
+
+
+@app.route('/api')
+def my_microservice():
+    print(request)
+    pprint(request.environ)
+
+    response = jsonify({'Hello': 'World!'})
+
+    print(response)
+    print(response.data)
+
+    return response
+
+
+@app.route('/api/person/<registered:name>')
+def person(name):
+    response = jsonify({'Hello': name})
+    return response
+
+
+@app.route('/')
+def auth():
+    print('The raw Authorization header')
+    print(request.environ['HTTP_AUTHORIZATION'])
+    print("Flask's Authorization header")
+    print(request.authorization)
+    return ""
+
+
+if __name__ == '__main__':
+    print('**********************************************')
+    print('** Starting app:')
+    print(app.url_map)
+    print('**********************************************')
+    app.run()
